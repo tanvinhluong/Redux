337e18c68fd129373d05e391184d317ce3f8bd70
Vinh Luong <vinhluong1722@gmail.com>
2024-01-15 22:19:15 +0700

    commit code

---
+++ my-app/src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+
+function HelloWorld(){
+    return <h1>Hello World</h1>;
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(<HelloWorld />);
